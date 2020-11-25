eae4b6aa8837bece98637203a12015a03488cd68
vankhanhbds <72186716+vankhanhbds@users.noreply.github.com>
2020-11-25 07:19:09 +0700

    Add files via upload

---
+++ admins.php
@@ -0,0 +1,33 @@
+<?
+    // Initialize the session.
+    session_start();
+    
+    require_once("../inc/checkAdminPagePermissions.php");
+    require_once("../inc/config.inc.php");
+
+    
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+
+<html>
+<head>
+    <title><? echo _SITE_NAME; ?> :: Admin Panel :: Home</title>
+    <meta http-equiv=Content-Type content="text/html; charset=utf-8">
+    <link href="css/style_<? echo _CSS_STYLE;?>.css" type=text/css rel=stylesheet>
+</head>
+
+
+<!-- BEGIN MAIN CONTENT ARE -->
+<body style="background: #ffffff;">
+
+<h4>Account Manager -> Admins</h4>
+<hr style="color:#cccccc">
+
+    <br />    
+    <br />    
+    <center>{Add you php code here...}</center>
+
+
+</body>
+</html>
